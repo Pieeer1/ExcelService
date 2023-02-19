3026ea1f7ee56ebf649f01636cb1b0ebba832771
Pieeer1 <mason.adams@msn.com>
2023-02-19 10:59:08 -0800

    initial push

--- Tests/ExcelService.IntegrationTests/Program.cs
+++ Tests/ExcelService.IntegrationTests/Program.cs
@@ -10,74 +10,90 @@ namespace ExcelService.IntegrationTests
         {
             Excel excel = new Excel();
 
-            excel.GenerateNewWorkBook(new List<TestClass>()
+            Console.WriteLine("Welcome to The ExcelService Integrated Test Environment");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("Select Between the Following 2 options: ");
+            Console.WriteLine("1 - Create a workbook example");
+            Console.WriteLine("2 - Read a workbook example");
+            string? response = Console.ReadLine();
+
+            int responseInt = ParseReader(response).Invoke();
+            if (responseInt == 1)
             {
-                new TestClass("a", "b", "c", "d", "e", "f", "g", 1, new DateTime(1999, 12, 08)),
-                new TestClass("h", "i", "j", "k", "l", "m", "m", 2, new DateTime(1998, 11, 07)),
-                new TestClass("n", "l", "o", "p", "q", "r", "s", 2, new DateTime(1998, 11, 07)),
-            },
-            new List<List<Style>>()
+
+                excel.GenerateNewWorkBook(new List<TestClass>()
+                {
+                    new TestClass("a", "b", "c", "d", "e", "f", "g", 1, new DateTime(1999, 12, 08)),
+                    new TestClass("h", "i", "j", "k", "l", "m", "m", 2, new DateTime(1998, 11, 07)),
+                    new TestClass("n", "l", "o", "p", "q", "r", "s", 2, new DateTime(1998, 11, 07)),
+                },
+                new List<List<Style>>()
+                {
+                    new List<Style>()
+                    {
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                    },
+                    new List<Style>()
+                    {
+                        new Style(Enums.Font.Calibri, Color.Red, 8),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        new Style(Enums.Font.Calibri, Color.Red, 8),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                    },
+                    new List<Style>()
+                    {
+                        new Style(Enums.Font.Calibri, Color.Pink, 8),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        new Style(Enums.Font.Arial, Color.Green, 20),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                    },
+                    new List<Style>()
+                    {
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                        new Style(Enums.Font.Arial, Color.Red, 25),
+                        new Style(Enums.Font.Arial, Color.Yellow, 48),
+                        Style.Empty(),
+                        Style.Empty(),
+                        Style.Empty(),
+                    },
+                },
+                "TestWorkbook",
+                "TestSheet");
+
+                excel["TestWorkbook"]?.StyleRowWhere<TestClass>(x => x.Column1 == "a", new Style(Enums.Font.Arial, Color.Green, 65));
+
+                excel["TestWorkbook"]?.StyleRowWhere<TestClass>(x => x.Column1 == "n", new Style(Enums.Font.Arial, Color.Red, 25));
+
+                excel["TestWorkbook"]?.StyleCellWhere<TestClass>(x => x.Column2 == "b", new Style(Enums.Font.Calibri, Color.Aqua, 30));
+
+                excel.SaveExcelFileFromWorkbook("../../../test.xlsx", excel["TestWorkbook"] ?? throw new NullReferenceException("Invalid Container"));
+
+            }
+            else if (responseInt == 2)
             { 
-                new List<Style>()
-                { 
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                },
-                new List<Style>()
-                {
-                    new Style(Enums.Font.Calibri, Color.Red, 8),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    new Style(Enums.Font.Calibri, Color.Red, 8),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                },
-                new List<Style>()
-                {
-                    new Style(Enums.Font.Calibri, Color.Pink, 8),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    new Style(Enums.Font.Arial, Color.Green, 20),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                },
-                new List<Style>()
-                {
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                    new Style(Enums.Font.Arial, Color.Red, 25),
-                    new Style(Enums.Font.Arial, Color.Yellow, 48),
-                    Style.Empty(),
-                    Style.Empty(),
-                    Style.Empty(),
-                },
-            },
-            "TestWorkbook",
-            "TestSheet");
-
-            excel["TestWorkbook"]?.StyleRowWhere<TestClass>(x => x.Column1 == "a", new Style(Enums.Font.Arial, Color.Green, 65));
-
-            excel["TestWorkbook"]?.StyleRowWhere<TestClass>(x => x.Column1 == "n", new Style(Enums.Font.Arial, Color.Red, 25));
-
-            excel["TestWorkbook"]?.StyleCellWhere<TestClass>(x => x.Column2 == "b", new Style(Enums.Font.Calibri, Color.Aqua, 30));
-
-            excel.SaveExcelFileFromWorkbook("../../../test.xlsx", excel["TestWorkbook"] ?? throw new NullReferenceException("Invalid Container"));
-            
+                //read workbook
+            }
         }
 
         public class TestClass
@@ -104,6 +120,21 @@ namespace ExcelService.IntegrationTests
             public string? Column7 { get; set; }
             public int Column8 { get; set; }
             public DateTime? Column9 { get; set; }
+        }
+
+        private static Func<int> ParseReader(string? rawInput)
+        {
+            return rawInput switch
+            {
+                "1" => () => { return 1;},
+                "2" => () => { return 2;},
+                _ => () => 
+                { 
+                    Console.WriteLine("Invalid Input, Please Input 1 or 2:");
+                    string? newInput = Console.ReadLine(); 
+                    return ParseReader(newInput).Invoke(); 
+                }
+            };
         }
 
 
